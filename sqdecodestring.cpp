7fb58887bda41ac555d80a8debea47e11426e169
gouravsinghal <49369629+gouravsinghal@users.noreply.github.com>
2021-01-11 21:29:36 +0530

    Create sqdecodestring.cpp
    Given an encoded string, return its decoded string.
    
    The encoding rule is: k[encodedstring], where the encodedstring inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
    
    You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
    
    Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
    
    Input Format
    First line contains the string s.
    
    Constraints
    None
    
    Output Format
    Print the decoded string
    
    Sample Input
    3[a]2[bc]3[ee]2[r]
    Sample Output
    aaabcbceeeeeerr
    Explanation
    None

---
+++ sqdecodestring.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+string decode(string s)
+{
+    string ans="",temp="";
+    stack<pair<string,int>> st;
+    int i=0;
+    while(i<s.length())
+    {
+        if(isdigit(s[i])){
+            int num=0;
+            while(isdigit(s[i])){
+                int tempnum=s[i]-'0';
+                num=num*10+tempnum;
+                i++;
+            }
+            st.push(make_pair(temp,num));
+            temp="";
+        }
+        else if(isalpha(s[i])) temp+=s[i];
+        else if(s[i]==']')
+        {
+            pair<string,int> p=st.top();
+            st.pop();
+            string cur=p.first;
+            for(int i=0;i<p.second;i++)
+            {
+                cur+=temp;
+            }
+            if(s.empty())
+            {
+                ans+=cur;
+                temp="";
+            }
+            else
+            temp=cur;
+
+        }
+        i++;
+    }
+    ans+=temp;
+    return ans;
+}
+int main() {
+    string s;
+    cin>>s;
+    cout<<decode(s);
+}
