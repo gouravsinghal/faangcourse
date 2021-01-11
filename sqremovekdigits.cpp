71a9b70ae37ec413a9b30af22946432178c08a92
gouravsinghal <49369629+gouravsinghal@users.noreply.github.com>
2021-01-11 21:01:04 +0530

    Create sqremovekdigits.cpp
    Given a non-negative integer N represented as a string, remove k digits from the number so that the new number is the smallest possible.
    
    Input Format
    Take Input N and K
    
    Constraints
    Note:
    1.The length of N is less than 10002 and will be ≥ k.
    2.The given N does not contain any leading zero.
    
    Output Format
    Print a new number that is the smallest
    
    Sample Input
    1432219 3
    Sample Output
    1219

---
+++ sqremovekdigits.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+string removeKdigits(string num, int k) {
+      stack<int> st;
+      string ret = "";
+      int n = num.size();
+      for(int i = 0; i < n; i++){
+         while(k && !st.empty() && st.top() > num[i]){
+            st.pop();
+            k--;
+         }
+         st.push(num[i]);
+      }
+      while(k--)st.pop();
+      while(!st.empty()){
+         ret += st.top();
+         st.pop();
+      }
+      reverse(ret.begin(), ret.end());
+      string ans = "";
+      int i = 0;
+      while(i <ret.size() && ret[i] == '0')i++;
+      for(; i < ret.size(); i++)ans += ret[i];
+      ret = ans;
+      return ret.size() == 0? "0" : ret;
+   }
+int main() {
+    string s;
+    cin>>s;
+    int k;
+    cin>>k;
+    cout<<removeKdigits(s,k);
+}
